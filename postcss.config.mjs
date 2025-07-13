af1ccae1260d10afc099718a3c1a213379186c6a
Ansh Parmar <pranamiansh12@gmail.com>
2025-07-13 17:09:07 +0530

    remove theme field

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,5 @@
 const config = {
-  theme: {
-    extend: {
-      colors: {
-        background: "#ffffff",
-        text: "#000000",
-        // etc.
-      },
-    },
-  },
   plugins: ["@tailwindcss/postcss"],
-  experimental: {
-    disableColorOkLCH: true, // ⛔ disables oklch() colors
-  },
 };
 
 export default config;
