052b13cb6822242d7a3ce52a466b83d178dec2f6
Ansh Parmar <pranamiansh12@gmail.com>
2025-07-13 18:22:02 +0530

    Add comment

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -3,6 +3,7 @@ import { inngest } from "@/lib/inngest/client"
 import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 // Create an API that serves zero functions
+// create an API
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
